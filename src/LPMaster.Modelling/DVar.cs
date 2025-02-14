fd51c299a5d55394190f3dcb09a056554ec28d12
hakanttek <hakanttek@gmail.com>
2025-02-14 15:29:37 +0100

    feat(Multi): Crated to handle multipication process

--- src/LPMaster.Modelling/DVar.cs
+++ src/LPMaster.Modelling/DVar.cs
@@ -2,6 +2,8 @@
 
 public class DVar
 {
+    public int Id { get; set; }
+
     public int ModelId { get; init; }
 
     public int ColIndex { get; init; }
---
+++ src/LPMaster.Modelling/Multi.cs
@@ -0,0 +1,12 @@
+﻿namespace LPMaster.Modelling;
+
+class Multi
+{
+    public double Coef { get; init; }
+    
+    public double DVarId { get; init; }
+
+    public required DVar DVar { get; init; }
+
+    public int ExpressionId { get; init; }
+}
