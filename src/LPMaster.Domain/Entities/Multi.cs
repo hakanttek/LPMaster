d4a8bfb1bbc45779f065292f570111e78704e336
hakanttek <hakanttek@gmail.com>
2025-02-14 23:25:26 +0100

    refactor(Multi): Made DVarId and DVar nullable to disable constant values.  - Added IsConstant property that returns true if DVarId is null, false otherwise.

--- src/LPMaster.Domain/Entities/Multi.cs
+++ src/LPMaster.Domain/Entities/Multi.cs
@@ -4,9 +4,11 @@ public class Multi
 {
     public double Coef { get; init; }
     
-    public double DVarId { get; init; }
+    public double? DVarId { get; init; }
 
-    public required DVar DVar { get; init; }
+    public required DVar? DVar { get; init; }
 
     public int ExpressionId { get; init; }
+
+    public bool IsConstant => DVarId is null;
 }
