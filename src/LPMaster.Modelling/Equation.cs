fd4ddfc336549472c446bc9b20c56c054cea9fbe
hakanttek <hakanttek@gmail.com>
2025-02-14 15:39:51 +0100

    feat(Equation): Created to handle mathematical equations (constraints in model)

---
+++ src/LPMaster.Modelling/Equation.cs
@@ -0,0 +1,16 @@
+﻿namespace LPMaster.Modelling;
+
+class Equation
+{
+    public int Id { get; init; }
+
+    public int LeftExpressionId { get; init; }
+
+    public required Expression LeftExpression { get; init; }
+
+    public int RightExpressionId { get; init; }
+
+    public required Expression RightExpression { get; init; }
+
+    public int Relation { get; init; }
+}
