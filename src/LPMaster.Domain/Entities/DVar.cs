39285087a51f35d097a45cc6dd04b7092fe0ca60
hakanttek <hakanttek@gmail.com>
2025-02-15 03:05:07 +0100

    refactor(DVar ):remove IUnique implementation and add equality overrides - Overrided `Equals` and `GetHashCode` methods for value-based equality on `ModelId` and `ColIndex`.

--- src/LPMaster.Domain/Entities/DVar.cs
+++ src/LPMaster.Domain/Entities/DVar.cs
@@ -3,10 +3,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LPMaster.Domain.Entities;
 
-public class DVar : IUnique<int>, INameable, IDescribable
+public class DVar : INameable, IDescribable
 {
-    public int Id { get; init; }
-
     public int ModelId { get; init; }
 
     [ForeignKey(nameof(ModelId))]
@@ -17,4 +15,10 @@ public class DVar : IUnique<int>, INameable, IDescribable
     public string? Name { get; init; }
 
     public string? Description { get; init; }
+
+    public override bool Equals(object? obj) => obj is DVar dvar
+        && dvar.ModelId == ModelId
+        && dvar.ColIndex == ColIndex;
+
+    public override int GetHashCode() => HashCode.Combine(ModelId, ColIndex);
 }
