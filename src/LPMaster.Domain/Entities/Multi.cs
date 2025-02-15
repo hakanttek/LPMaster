483f6c55925efe7a50fff77c73562e53799104b1
hakanttek <hakanttek@gmail.com>
2025-02-15 16:22:49 +0100

    feat(Core.Test): Created test project for Core projects (Domain and Application).  - Created test for IsConstant, ModelId and Model methods of Multi  - Added ForeignKey attribute to DVar property of Multi  - Updated to IsConstant method to check nulability of DVar instead of DVarId

--- src/LPMaster.Domain/Entities/Multi.cs
+++ src/LPMaster.Domain/Entities/Multi.cs
@@ -1,4 +1,6 @@
-﻿namespace LPMaster.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LPMaster.Domain.Entities;
 
 public class Multi
 {
@@ -6,11 +8,12 @@ public class Multi
     
     public double? DVarId { get; init; }
 
-    public required DVar? DVar { get; init; }
+    [ForeignKey(nameof(DVarId))]
+    public DVar? DVar { get; init; }
 
     public int ExpressionId { get; init; }
 
-    public bool IsConstant => DVarId is null;
+    public bool IsConstant => DVar is null;
 
     public int? ModelId => DVar?.ModelId;
 
---
+++ tests/LPMaster.Core.Tests/Entities/MultiTests.cs
@@ -0,0 +1,67 @@
+﻿using LPMaster.Domain.Entities;
+
+namespace LPMaster.Core.Tests.Entities;
+
+public class MultiTests
+{
+    private DVar _dvar;
+
+    [SetUp]
+    public void Setup()
+    {
+        var oFunc = new Expression() { Multis = [] };
+        var model = new Model() { Constraints = [], ObjectiveFunction = oFunc };
+        _dvar = new DVar() { Model = model };
+    }
+
+    [Test]
+    [TestCase(true, false, TestName = "DVarExists_IsConstantShouldBeFalse")]
+    [TestCase(false, true, TestName = "DVarDoesNotExist_IsConstantShouldBeTrue")]
+    public void IsConstant_ReturnsExpectedValue(bool hasDVar, bool expectedIsConstant)
+    {
+        // Arrange
+        var multi = new Multi()
+        {
+            DVarId = hasDVar ? _dvar.Id : null,
+            DVar = hasDVar ? _dvar : null
+        };
+
+        // Act
+        var isConstant = multi.IsConstant;
+
+        // Assert
+        Assert.That(isConstant, Is.EqualTo(expectedIsConstant));
+    }
+
+    [Test]
+    [TestCase(true, TestName = "DVarExists_ShouldMatchModelIdAndModel")]
+    [TestCase(false, TestName = "DVarDoesNotExist_ShouldModelIdAndModelBeNull")]
+    public void ModelIdAndModel_ShouldBehaveAsExpectedBasedOnDVar(bool hasDVar)
+    {
+        // Arrange
+        var multi = new Multi()
+        {
+            DVarId = hasDVar ? _dvar.Id : null,
+            DVar = hasDVar ? _dvar : null
+        };
+
+        // Act
+        var modelId = multi.ModelId;
+        var model = multi.Model;
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            if (hasDVar)
+            {
+                Assert.That(modelId, Is.EqualTo(_dvar.ModelId));
+                Assert.That(model, Is.EqualTo(_dvar.Model));
+            }
+            else
+            {
+                Assert.That(modelId, Is.Null);
+                Assert.That(model, Is.Null);
+            }
+        });
+    }
+}
