a62a12d788677a31c5d333a89e09ddc582293a08
hakanttek <hakanttek@gmail.com>
2025-02-15 12:55:33 +0100

    Revert "refactor(DVar ):remove IUnique implementation and add equality overrides"
    This reverts commit 39285087a51f35d097a45cc6dd04b7092fe0ca60.

--- src/LPMaster.Domain/Entities/DVar.cs
+++ src/LPMaster.Domain/Entities/DVar.cs
@@ -3,8 +3,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LPMaster.Domain.Entities;
 
-public class DVar : INameable, IDescribable
+public class DVar : IUnique<int>, INameable, IDescribable
 {
+    public int Id { get; init; }
+
     public int ModelId { get; init; }
 
     [ForeignKey(nameof(ModelId))]
@@ -15,10 +17,4 @@ public class DVar : INameable, IDescribable
     public string? Name { get; init; }
 
     public string? Description { get; init; }
-
-    public override bool Equals(object? obj) => obj is DVar dvar
-        && dvar.ModelId == ModelId
-        && dvar.ColIndex == ColIndex;
-
-    public override int GetHashCode() => HashCode.Combine(ModelId, ColIndex);
 }
