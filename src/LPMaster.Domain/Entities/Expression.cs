8b8f79c2f0cc0ee80b7455ecc3faa40aa26ea79c
hakanttek <hakanttek@gmail.com>
2025-02-15 03:14:20 +0100

    feat(Expression): add Model property class to retrieve unique Model from Multis

--- src/LPMaster.Domain/Entities/Expression.cs
+++ src/LPMaster.Domain/Entities/Expression.cs
@@ -9,4 +9,11 @@ public class Expression : IUnique<int>, IDescribable
     public required IEnumerable<Multi> Multis { get; init; }
 
     public string? Description { get; init; }
+
+    public Model Model => Multis
+        .Where(multi => multi.ModelId is not null)
+        .Select(multi => multi.Model)
+        .Distinct()
+        .SingleOrDefault() ?? throw new InvalidOperationException(
+            $"No unique Model found for the given multipications. Expression Id: {Id}, Description: {(Description ?? "No description available")}");
 }
