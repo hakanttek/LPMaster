219a420b551d1b2e7ab8edc204365612606766cb
hakanttek <hakanttek@gmail.com>
2025-02-17 03:55:50 +0100

    feat(ModelTest): Creat with VerifyAll_ShouldReturnTrue test

---
+++ tests/LPMaster.Core.Tests/Entities/ModelTest.cs
@@ -0,0 +1,19 @@
+﻿using LPMaster.Domain.Entities;
+
+namespace LPMaster.Core.Tests;
+
+public class ModelTest
+{
+    [Test]
+    public void VerifyAll_ShouldReturnTrue()
+    {
+        // Assert
+        var model = Fake.Model;
+
+        // Act
+        var allVerified = model.VerifyAll();
+
+        // Assert
+        Assert.That(allVerified, Is.True);
+    }
+}
