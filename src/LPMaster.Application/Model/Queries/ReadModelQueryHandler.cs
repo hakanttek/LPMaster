2b705defa69bc29bd653c3cadff8596d2f587cc7
hakanttek <hakanttek@gmail.com>
2025-02-18 16:27:42 +0100

    feat(ReadModelQueryHandler): Created to handle model reading

---
+++ src/LPMaster.Application/Model/Queries/ReadModelQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using LPMaster.Application.Model.Dto;
+using MediatR;
+
+namespace LPMaster.Application.Model.Queries;
+
+public record ReadModelQuery(int? Id = null, int? Name = null) : IRequest<ModelReadDto>;
+
+public class ReadModelQueryHandler : IRequestHandler<ReadModelQuery, ModelReadDto>
+{
+    public Task<ModelReadDto> Handle(ReadModelQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
