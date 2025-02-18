6e2433e6b02c3ae37968864cb3b8862e0e195bf7
hakanttek <hakanttek@gmail.com>
2025-02-18 18:08:48 +0100

    feat(DependencyInjection): Created extension method to add required services for LPMaster.Application

---
+++ src/LPMaster.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace LPMaster.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddLPMasterApplication(this IServiceCollection services) => services
+        .AddAutoMapper(Assembly.GetExecutingAssembly())
+        .AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+}
