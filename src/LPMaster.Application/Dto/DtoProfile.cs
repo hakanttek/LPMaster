707cb9a4c3fe6b42e04e8fbedb3672b56385ed5c
hakanttek <hakanttek@gmail.com>
2025-02-18 17:29:51 +0100

    feat(DtoProfile): Create profile for the mapping of dtos

---
+++ src/LPMaster.Application/Dto/DtoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LPMaster.Application.Dto.Read;
+using LPMaster.Application.Dto.Update;
+using LPMaster.Domain.Entities;
+
+namespace LPMaster.Application.Dto;
+
+public class DtoProfile : Profile
+{
+    public DtoProfile()
+    {
+        CreateMap<Model, ModelReadDto>();
+        CreateMap<ModelUpdateDto, Model>();
+    }
+}
