ca2783fd84155e7b351baa32c5bc115701f5f3f5
hakanttek <hakanttek@gmail.com>
2025-02-19 16:03:08 +0100

    feat(DVarCreateDto): Added  required proporties.  - Assigned ModelId and ColIndex -1 by default.  - Added BoundToModel to check if ModelId and ColIndex properties are set.

--- src/LPMaster.Application/Dto/Create/DVarCreateDto.cs
+++ src/LPMaster.Application/Dto/Create/DVarCreateDto.cs
@@ -2,4 +2,13 @@
 
 public record DVarCreateDto()
 {
+    public int ModelId { get; internal set; } = -1;
+
+    public int ColIndex { get; internal set; } = -1;
+
+    public string? Name { get; init; }
+
+    public string? Description { get; init; }
+
+    public bool BoundToModel => ModelId >= 0 && ColIndex >= 0;
 }
