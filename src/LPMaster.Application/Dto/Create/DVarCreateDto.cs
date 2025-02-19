53d95b87293c5f3418b5ba87ac3d06b21aa89043
hakanttek <hakanttek@gmail.com>
2025-02-19 04:16:00 +0100

    feat(DVarCreateDto): Implemented to basic propoerty of DVar creation.  - Added thread-safty created dvar list to be able to find the index of dvar

--- src/LPMaster.Application/Dto/Create/DVarCreateDto.cs
+++ src/LPMaster.Application/Dto/Create/DVarCreateDto.cs
@@ -1,5 +1,28 @@
-﻿namespace LPMaster.Application.Dto.Create;
+﻿using LPMaster.Application.Dto.Read;
+
+namespace LPMaster.Application.Dto.Create;
 
 public record DVarCreateDto()
 {
+    public int ModelId => Model.Id;
+
+#pragma warning disable CS8618
+    private readonly ModelReadDto _model;
+#pragma warning restore CS8618
+
+    public required ModelReadDto Model
+    {
+        get => _model;
+        init
+        {
+            _model = value;
+            _model.Add(this);
+        }
+    }
+
+    public int ColIndex => _model.CreatedDvars.ToList().IndexOf(this);
+
+    public string? Name { get; init; }
+
+    public string? Description { get; init; }
 }
--- src/LPMaster.Application/Dto/Read/ModelReadDto.cs
+++ src/LPMaster.Application/Dto/Read/ModelReadDto.cs
@@ -1,7 +1,23 @@
-﻿using LPMaster.Application.Dto.Update;
+﻿using LPMaster.Application.Dto.Create;
+using LPMaster.Application.Dto.Update;
 
 namespace LPMaster.Application.Dto.Read;
 
 public record ModelReadDto : ModelUpdateDto
 {
+    public int Id { get; init; }
+
+    private readonly List<DVarCreateDto> _createdDvars = new();
+
+    internal IEnumerable<DVarCreateDto> CreatedDvars => _createdDvars;
+    
+    private readonly Lock _dvarAddingLocker = new();
+
+    internal void Add(DVarCreateDto dto)
+    {
+        lock (_dvarAddingLocker)
+        {
+            _createdDvars.Add(dto);
+        }
+    }
 }
