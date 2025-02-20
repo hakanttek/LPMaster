e7b64f2292757681242cb29d4b390312920dd1cd
hakanttek <hakanttek@gmail.com>
2025-02-20 01:10:37 +0100

    feat(CreateModelCommand): Added DVars and Model properties

--- src/LPMaster.Application/Models/Commands/CreateModelCommandHandler.cs
+++ src/LPMaster.Application/Models/Commands/CreateModelCommandHandler.cs
@@ -1,8 +1,9 @@
-﻿using MediatR;
+﻿using LPMaster.Application.Dto.Create;
+using MediatR;
 
 namespace LPMaster.Application.Models.Commands;
 
-public record CreateModelCommand : IRequest<int>;
+public record CreateModelCommand(IEnumerable<DVarCreateDto> DVars, ModelCreateDto Model) : IRequest<int>;
 
 public class CreateModelCommandHandler : IRequestHandler<CreateModelCommand, int>
 {
