e1690bb3e709256648bf91b40f285ddb66380f39
hakanttek <hakanttek@gmail.com>
2025-02-24 00:44:47 +0100

    feat(ModelTests): Add exception handling test for CreateModelCommand  - Introduced a new test method `CreateModelCommand_ShouldThrowExpectedException` in `ModelTests.cs` to validate exception handling for the `CreateModelCommand`. Added necessary using directives for `LPMaster.Application.Exceptions` and implemented multiple test cases to ensure the correct exception type is thrown based on the number of creation attempts.

--- tests/LPMaster.Core.Tests/Application/ModelTests.cs
+++ tests/LPMaster.Core.Tests/Application/ModelTests.cs
@@ -1,4 +1,5 @@
-﻿using LPMaster.Application.Models.Commands;
+﻿using LPMaster.Application.Exceptions;
+using LPMaster.Application.Models.Commands;
 using LPMaster.Application.Models.Queries;
 using LPMaster.Domain.Enums;
 using MediatR;
@@ -37,5 +38,33 @@ public class ModelTests
             Assert.That(model.Id, Is.EqualTo(res.Id));
             Assert.That(model.Name, Is.EqualTo(res.Name));
         });        
+    }
+
+    [Test]
+    [TestCase(1)]
+    [TestCase(2, typeof(BadRequestException))]
+    public async Task CreateModelCommand_ShouldThrowExpectedException(int numberOfCreation, Type? expectedExceptionType = null)
+    {
+        Type? exceptionType = null;
+        try
+        {
+            // Arrange
+            var modelName = Guid.NewGuid().ToString();
+            var createCommand = new CreateModelCommand()
+            {
+                Objective = Objective.Minimization,
+                Name = modelName
+            };
+
+            // Act
+            for (int i = 0; i < numberOfCreation; i++)
+                await _mediator.Send(createCommand);
+        }
+        catch (Exception ex)
+        {
+            exceptionType = ex.GetType();
+        }
+
+        Assert.That(exceptionType, Is.EqualTo(expectedExceptionType));
     }
 }
