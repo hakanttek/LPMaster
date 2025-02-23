aef98e7d3330e304b34267d176735370d62148bf
hakanttek <hakanttek@gmail.com>
2025-02-23 20:46:26 +0100

    refactor(ReadModelQueryHandler): Implemented logic in Handle method to retrieve models and map to ModelReadDto.

--- src/LPMaster.Application/Models/Queries/ReadModelQueryHandler.cs
+++ src/LPMaster.Application/Models/Queries/ReadModelQueryHandler.cs
@@ -1,14 +1,21 @@
-﻿using LPMaster.Application.Dto.Read;
+﻿using AutoMapper;
+using LPMaster.Application.Contracts.Repositories;
+using LPMaster.Application.Contracts.Repositories.Base;
+using LPMaster.Application.Dto.Read;
 using MediatR;
 
 namespace LPMaster.Application.Models.Queries;
 
 public record ReadModelQuery(int? Id = null, string? Name = null) : IRequest<ModelReadDto>;
 
-public class ReadModelQueryHandler : IRequestHandler<ReadModelQuery, ModelReadDto>
+public class ReadModelQueryHandler(IModelRepository repository, IMapper mapper) : IRequestHandler<ReadModelQuery, ModelReadDto>
 {
-    public Task<ModelReadDto> Handle(ReadModelQuery request, CancellationToken cancellationToken)
+    public async Task<ModelReadDto> Handle(ReadModelQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var model = request.Id is null
+            ? await repository.ReadFirstAsync(m => m.Name == request.Name, cancellationToken: cancellationToken)
+            : await repository.ReadFirstAsync(m => m.Id == request.Id, cancellationToken: cancellationToken);
+
+        return mapper.Map<ModelReadDto>(model);
     }
 }
