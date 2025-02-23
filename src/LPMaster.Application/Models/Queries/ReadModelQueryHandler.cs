ecc511ab2b00f0c7782d6d093645f2b926ffe5d7
hakanttek <hakanttek@gmail.com>
2025-02-23 23:43:29 +0100

    Add validation and error handling in ReadModelQueryHandler
    Introduce checks to ensure either Id or Name is provided in the ReadModelQuery request. Throw BadRequestException for invalid input and NotFoundException if the model is not found during retrieval.

--- src/LPMaster.Application/Models/Queries/ReadModelQueryHandler.cs
+++ src/LPMaster.Application/Models/Queries/ReadModelQueryHandler.cs
@@ -2,6 +2,7 @@
 using LPMaster.Application.Contracts.Repositories;
 using LPMaster.Application.Contracts.Repositories.Base;
 using LPMaster.Application.Dto.Read;
+using LPMaster.Application.Exceptions;
 using MediatR;
 
 namespace LPMaster.Application.Models.Queries;
@@ -12,9 +13,15 @@ public class ReadModelQueryHandler(IModelRepository repository, IMapper mapper) 
 {
     public async Task<ModelReadDto> Handle(ReadModelQuery request, CancellationToken cancellationToken)
     {
-        var model = request.Id is null
-            ? await repository.ReadFirstAsync(m => m.Name == request.Name, cancellationToken: cancellationToken)
-            : await repository.ReadFirstAsync(m => m.Id == request.Id, cancellationToken: cancellationToken);
+        if(request.Id is null && request.Name is null)
+            throw new BadRequestException("Id or Name must be provided");
+        else if(request.Id is not null && request.Name is not null)
+            throw new BadRequestException("Only one of Id or Name must be provided");
+
+        var model = (request.Id is null
+                ? await repository.ReadFirstAsync(m => m.Name == request.Name, cancellationToken: cancellationToken)
+                : await repository.ReadFirstAsync(m => m.Id == request.Id, cancellationToken: cancellationToken))
+                ?? throw new NotFoundException("Model not found");
 
         return mapper.Map<ModelReadDto>(model);
     }
