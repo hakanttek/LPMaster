6f2dd0a8857deed25d08696bbd9c3ea44a1108cb
hakanttek <hakanttek@gmail.com>
2025-02-24 16:00:47 +0100

    feat(DeleteModelCommandHandler): Create deletion logic to be able to pass unit tests

--- src/LPMaster.Application/Models/Commands/DeleteModelCommandHandler.cs
+++ src/LPMaster.Application/Models/Commands/DeleteModelCommandHandler.cs
@@ -1,13 +1,28 @@
-﻿using MediatR;
+﻿using LPMaster.Application.Contracts.Repositories;
+using LPMaster.Application.Contracts.Repositories.Base;
+using LPMaster.Application.Exceptions;
+using MediatR;
 
 namespace LPMaster.Application.Models.Commands;
 
 public record DeleteModelCommand(int? Id= null, string? Name = null) : IRequest;
 
-public class DeleteModelCommandHandler : IRequestHandler<DeleteModelCommand>
+public class DeleteModelCommandHandler(IModelRepository repository) : IRequestHandler<DeleteModelCommand>
 {
-    public Task Handle(DeleteModelCommand request, CancellationToken cancellationToken)
+    public async Task Handle(DeleteModelCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        // Check if either Id or Name is provided
+        if (request.Id is null && request.Name is null)
+            throw new BadRequestException("Id or Name must be provided");
+        // Check if both Id and Name are provided
+        else if (request.Id is not null && request.Name is not null)
+            throw new BadRequestException("Only one of Id or Name must be provided");
+
+        var model = (request.Id is null
+                ? await repository.ReadFirstAsync(m => m.Name == request.Name, tracked: true, cancellationToken: cancellationToken)
+                : await repository.ReadFirstAsync(m => m.Id == request.Id, tracked: true, cancellationToken: cancellationToken))
+                ?? throw new NotFoundException("Model not found");
+
+        await repository.DeleteAsync(model, cancellationToken);
     }
 }
