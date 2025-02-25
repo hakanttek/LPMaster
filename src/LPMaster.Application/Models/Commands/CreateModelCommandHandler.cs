577c6d59f5eeb675e92f804a14c6a8cc683bf1f0
hakanttek <hakanttek@gmail.com>
2025-02-25 03:44:13 +0100

    Refactor model creation validation logic
    Removed name uniqueness check from CreateModelCommandHandler.
    Introduced CreateModelCommandValidator to handle validation
    using FluentValidation, improving code structure by separating
    validation from command handling.

--- src/LPMaster.Application/Models/Commands/CreateModelCommandHandler.cs
+++ src/LPMaster.Application/Models/Commands/CreateModelCommandHandler.cs
@@ -1,7 +1,5 @@
 ﻿using AutoMapper;
 using LPMaster.Application.Common.Contracts.Repositories;
-using LPMaster.Application.Common.Contracts.Repositories.Base;
-using LPMaster.Application.Common.Exceptions;
 using LPMaster.Domain.Entities;
 using LPMaster.Domain.Enums;
 using MediatR;
@@ -16,10 +14,6 @@ public class CreateModelCommandHandler(IMapper mapper, IModelRepository reposito
 {
     public async Task<CreateModelResult> Handle(CreateModelCommand request, CancellationToken cancellationToken)
     {
-        // Check if a model with the same name already exists
-        if (await repository.AnyAsync(m => m.Name == request.Name, cancellationToken))
-            throw new BadRequestException("Model with the same name already exists.");
-
         var model = mapper.Map<LinModel>(request);
         var createdModel = await repository.CreateAsync(model, cancellationToken);
         return new CreateModelResult(createdModel.Id, createdModel.Name);
---
+++ src/LPMaster.Application/Models/Commands/CreateModelCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using LPMaster.Application.Common.Contracts.Repositories;
+using LPMaster.Application.Common.Contracts.Repositories.Base;
+
+namespace LPMaster.Application.Models.Commands;
+
+public class CreateModelCommandValidator : AbstractValidator<CreateModelCommand>
+{
+    public CreateModelCommandValidator(IModelRepository repository)
+    {
+        RuleFor(model => model.Name)
+            .MustAsync(async (name, cancellationToken) => !await repository.AnyAsync(m => m.Name == name, cancellationToken));
+    }
+}
